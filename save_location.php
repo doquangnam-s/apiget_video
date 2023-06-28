3944200fb6e3228cd106e4ebc136b16376e075a6
doquangnam-s <52003984+doquangnam-s@users.noreply.github.com>
2023-06-28 07:31:42 -0700

    Create save_location.php

---
+++ save_location.php
@@ -0,0 +1,17 @@
+<?php
+// Lấy thông tin vị trí từ yêu cầu AJAX
+$latitude = $_POST['latitude'];
+$longitude = $_POST['longitude'];
+
+// Tạo nội dung để lưu vào file
+$fileContent = "Vĩ độ: " . $latitude . "\nKinh độ: " . $longitude;
+
+// Đường dẫn tới file
+$filePath = 'id.txt';
+
+// Ghi nội dung vào file
+file_put_contents($filePath, $fileContent);
+
+// Phản hồi về trình duyệt
+echo "Lưu vị trí thành công!";
+?>
